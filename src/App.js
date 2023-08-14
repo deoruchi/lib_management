8e0185cf3068328d7bb2c14db6837de404d032c0
kurokurotho <deoruchi064@gmail.com>
2023-08-14 06:10:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -7,22 +7,24 @@ import Books from "./Components/bookComponents/Books";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { store } from "./StateManagement/store";
 import { Provider } from "react-redux";
-import { Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <Routes>
-          <Route path="/" element={<Main />}>
-            <Route index element={<Books />} />
-            <Route path="/member" element={<Members />}></Route>
-            <Route path="/transaction" element={<Transaction />}>
-              <Route index element={<Tr1 />}></Route>
-              <Route path="/transaction/debt" element={<Tr2 />} />
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<Main />}>
+              <Route index element={<Books />} />
+              <Route path="/member" element={<Members />}></Route>
+              <Route path="/transaction" element={<Transaction />}>
+                <Route index element={<Tr1 />}></Route>
+                <Route path="/transaction/debt" element={<Tr2 />} />
+              </Route>
             </Route>
-          </Route>
-        </Routes>
+          </Routes>
+        </HashRouter>
       </Provider>
       ,
     </div>
